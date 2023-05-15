74920262c8d716fc91c79ad5c746be88c9e31c6b
Santacion <santacionx@gmail.com>
2023-05-15 20:13:39 +0530

    read again documented insertion code

--- HASHMAP/hashmap.java
+++ HASHMAP/hashmap.java
@@ -2,6 +2,7 @@ package HASHMAP;
 
 import java.util.*;
 
+// linked list to store inside bucket array  (collision avoidance )
 class node<k, v> {
     k key;
     v value;
@@ -15,31 +16,51 @@ class node<k, v> {
 
 }
 
+// generic class
 public class hashmap<k, v> {
-    ArrayList<node<k, v>> buckets;
+    // note : array can't store generic @ compile time so use arraylist
+    // array list / bucket array stores in the form of seperate chaining (linked
+    // list used intenally if collision occurs )
+    // so class node linked list created
+    // size of bucket default set to 20
+
+    ArrayList<node<k, v>> bucket;
     int count, nbuckets;
 
     hashmap() {
 
-        buckets = new ArrayList<>();
+        bucket = new ArrayList<>();
+        // default set bucket size and place all null inside the bucket
         nbuckets = 20;
         for (int i = 0; i <= nbuckets; i++) {
-            buckets.add(null);
+            bucket.add(null);
         }
 
     }
+    // to insert string key into array particular index use hash function ex: a[0]
+    // place the value similar action for strings we need
 
     private int getBucketIndex(k key) {
         // hash function -> hash code + compression to place into bucked
+        // hashcode : converts strings into integer
         int hcode = key.hashCode();
+        // compress so the key is placed within the bucket/ array size
         int compress = hcode % nbuckets;
         return compress;
     }
 
     public void insert(k key, v value) {
+        // ask the getbucketindex to get the index to store the key
         int BucketIndex = getBucketIndex(key);
-        // search key is already present in list
-        node<k, v> head = buckets.get(BucketIndex);
+        // search key is already present in linked list that is inside the bucket
+        // access the bucketindex say ex: a[0] traverse inside the a[0] linked list to
+        // check if key present : true then place the value again;
+        // see bucket/arraylist holds node<k,v> type .get return the node<k,v> type arry
+        // this was i got confused how integer returned array holds the node<k,v> head
+        // but array return is **** node type ****
+
+        node<k, v> head = bucket.get(BucketIndex);
+        // array index
         while (head != null) {
             if (head.key.equals(key)) {
                 head.value = value;
@@ -47,8 +68,13 @@ public class hashmap<k, v> {
             head = head.next;
 
         }
-        // create a new node if no element
-        head = buckets.get(BucketIndex);
+        // get the a[0] once i couldnt find the search till null again make the head
+        // point to the initial index
+        // say a[0] contains no key or linked list then
+        // create list/node attach newnode next the head
+        // place this newnode into the a[0]
+
+        head = bucket.get(BucketIndex);
         node<k, v> newnode = new node<>(key, value);
         newnode.next = head;
 
